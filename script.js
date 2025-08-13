c2dac6f1d214b6391d88c4b4abf36dd04e0aa0c9
Dasnapplel <DiegoDJBV@hotmail.com>
2025-08-13 16:17:05 -0400

    updates
    yeah

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ window.onscroll = () => {
     });
 };
 
-
 //Burger menu 
 const hamMenu = document.querySelector('.ham-menu');
 const sideBar = document.querySelector('.side-bar')
@@ -46,7 +45,53 @@ const NavMenu = document.querySelector('.portait-nav-menu')
 
 NavBtn.addEventListener('click', () => {
     NavMenu.classList.toggle('active');
+    NavBtn.style.animation = 'none';
+    if (NavBtn.innerHTML.includes('↑')) {
+        NavBtn.innerHTML = NavBtn.innerHTML.replace('↑', '↓');
+    } else if (NavBtn.innerHTML.includes('↓')) {
+        NavBtn.innerHTML = NavBtn.innerHTML.replace('↓', '↑');
+    }
     for (let i=0; i < NavMenu.children.length; i++) {
         const child = NavMenu.children[i]
         child.classList.toggle('active');
     }});
+
+//projects page slide btns
+const sectionContent = document.querySelectorAll('.section-content');
+
+sectionContent.forEach(content => {
+    let leftBtn = content.querySelector('.left-btn');
+    let rightBtn = content.querySelector('.right-btn');
+    let slides = content.querySelector('.slides-container').children;
+    let currentSlide = 0;
+    let debounce = false;
+
+    leftBtn.addEventListener('click', () => {
+        if (debounce) return;
+        debounce = true;
+
+        slides[currentSlide].style.animation = 'fadeout 0.25s forwards';
+        slides[(currentSlide - 1 + slides.length) % slides.length].style.animation = 'fadein 0.125s';
+
+        setTimeout(() => {
+            slides[currentSlide].style.display = 'none';
+            currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+            slides[currentSlide].style.display = 'block';
+            debounce = false;
+        }, 250); // time in ms
+    });
+    rightBtn.addEventListener('click', () => {
+        if (debounce) return;
+        debounce = true;
+
+        slides[currentSlide].style.animation = 'fadeout 0.25s forwards';
+        slides[(currentSlide + 1 + slides.length) % slides.length].style.animation = 'fadein 0.125s';
+
+        setTimeout(() => {
+            slides[currentSlide].style.display = 'none';
+            currentSlide = (currentSlide + 1 + slides.length) % slides.length;
+            slides[currentSlide].style.display = 'block';
+            debounce = false;
+        }, 250); // time in ms
+    });
+});
