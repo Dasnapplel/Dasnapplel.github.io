5aa7b0939effb207089eb1422512e7c7705b455c
Dasnapplel <DiegoDJBV@hotmail.com>
2026-05-14 11:52:08 -0400

    many minor updates and bug fixes
    renamed variables and classes to better names
    organized all code
    fixed light mode toggle bug and nav bar bugs
    Added atomic object hackathon to projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dasnapplel.github.io
 Dasnapplel's project webpage for employers.
+
+Link to documentation: https://docs.google.com/document/d/1UbpaIo3AJw239vPrXw6U3AKehAeBnsUHis3gRDYpWDA/edit?usp=sharing
--- script.js
+++ script.js
@@ -1,100 +1,116 @@
-//Nav Bar link activations
+// Variables
 let sections = document.querySelectorAll('section');
 let resumes = document.querySelectorAll('.resume');
-let navLinks = document.querySelectorAll('.head-contain a');
-const NavMenu = document.querySelector('.portrait-nav-menu')
+let landscapeNavLinks = document.querySelectorAll('.landscape-navbar a');
+const portraitNavMenu = document.querySelector('.portrait-navbar-menu')
 
+const sideBarOpen = document.querySelector('.sidebar-open');
+const sideBar = document.querySelector('.sidebar');
+const sideBarClose = document.querySelector('.sidebar-close');
+
+const portraitNavmenuOpen = document.querySelector('.portrait-navbar-p2')
+
+const lightToggleBtn = document.querySelector('.light-toggle-btn');
+
+
+// Dark Mode Toggle
+const enterState = localStorage.getItem('DarkMode') === 'true';
+if (enterState) {
+    lightToggleBtn.classList.add('active');
+    lightToggleBtn.innerHTML = '☽';
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.add('dark-mode');
+    })
+}
+
+lightToggleBtn.addEventListener('click', () => {
+    lightToggleBtn.classList.toggle('active');
+    lightToggleBtn.innerHTML = lightToggleBtn.classList.contains('active') ? '☽' : '☼';
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.toggle('dark-mode');
+    })
+    if (localStorage.getItem('DarkMode') === 'true') {
+        localStorage.setItem('DarkMode', false);
+    } else {
+        localStorage.setItem('DarkMode', true);
+    }
+});
+
+
+// Nav Bar Link Activations
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 300;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        let title = document.querySelector('.portrait-head-p2');
+        let title = document.querySelector('.portrait-navbar-p2');
 
         if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
+            landscapeNavLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('.head-contain a[href*=' + id + ']').classList.add('active');
+                document.querySelector('.landscape-navbar a[href*=' + id + ']').classList.add('active');
                 title.innerHTML = id + ' ▼';
-                NavMenu.classList.remove('active');
+                portraitNavMenu.classList.remove('active');
             });
         };
     });
-    resumes.forEach(res => {
-        let top = window.scrollY;
-        let offset = res.offsetTop - 300;
-        let height = res.offsetHeight;
-        let id = res.getAttribute('id');
-        let title = document.querySelector('.portrait-head-p2');
+    // potential feature for multiple resumes in the resume section
+    // resumes.forEach(res => {
+    //     let top = window.scrollY;
+    //     let offset = res.offsetTop - 300;
+    //     let height = res.offsetHeight;
+    //     let id = res.getAttribute('id');
+    //     let title = document.querySelector('.portrait-navbar-p2');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('.head-contain a[href*=' + id + ']').classList.add('active');
-                title.innerHTML = id + ' ▼';
-                NavMenu.classList.remove('active');
-            });
-        };
-    });
-};
-
-//Burger menu 
-const hamMenu = document.getElementsByTagName('nav');
-const sideBar = document.querySelector('.side-bar');
-const sideBarClose = document.querySelector('.side-bar-close');
-
-for (let i=0; i < sideBar.children.length; i++) {
-    const child = sideBar.children[i]
-    if(child in document.querySelectorAll('side-bar a')) {
-        child.addEventListener('click', () => {
-            sideBar.classList.remove('active');
-        })};
+    //     if(top >= offset && top < offset + height) {
+    //         landscapeNavLinks.forEach(links => {
+    //             links.classList.remove('active');
+    //             document.querySelector('.landscape-navbar a[href*=' + id + ']').classList.add('active');
+    //             title.innerHTML = id + ' ▼';
+    //             portraitNavMenu.classList.remove('active');
+    //         });
+    //     };
+    // });
 };
 
 
-hamMenu[0].addEventListener('click', () => {
+// Sidebar Open and Close
+sideBarOpen.addEventListener('click', () => {
     sideBar.classList.add('active');
 });
 sideBarClose.addEventListener('click', () => {
     sideBar.classList.remove('active');
 });
+// redundent feature that closes the side bar after traveling pages
+// for (let i=0; i < sideBar.children.length; i++) {
+//     const child = sideBar.children[i]
+//     if(child in document.querySelectorAll('sidebar a')) {
+//         child.addEventListener('click', () => {
+//             sideBar.classList.remove('active');
+//         })};
+// };
 
-//portrait NavBar open
-const NavBtn = document.querySelector('.portrait-head-p2')
 
-NavBtn.addEventListener('click', () => {
-    // Change mode
-    if (NavBtn.innerHTML.includes('▲')) {
-        NavBtn.innerHTML = NavBtn.innerHTML.replace('▲', '▼');
-        NavMenu.classList.remove('active');
-    } else if (NavBtn.innerHTML.includes('▼')) {
-        NavBtn.innerHTML = NavBtn.innerHTML.replace('▼', '▲');
-        NavMenu.classList.add('active');
+// Portrait NavBar Menu Open
+portraitNavmenuOpen.addEventListener('click', () => {
+    if (portraitNavmenuOpen.innerHTML.includes('▲')) {
+        portraitNavmenuOpen.innerHTML = portraitNavmenuOpen.innerHTML.replace('▲', '▼');
+        portraitNavMenu.classList.remove('active');
+    } else if (portraitNavmenuOpen.innerHTML.includes('▼')) {
+        portraitNavmenuOpen.innerHTML = portraitNavmenuOpen.innerHTML.replace('▼', '▲');
+        portraitNavMenu.classList.add('active');
     }
     // 
-    // for (let i=0; i < NavMenu.children.length; i++) {
-    //     let child = NavMenu.children[i]
+    // for (let i=0; i < portraitNavMenu.children.length; i++) {
+    //     let child = portraitNavMenu.children[i]
     //     child.classList.toggle('active');
     // }
 });
 
-//dark mode toggle
-const lightToggleBtn = document.querySelector('.light-toggle-btn');
 
-lightToggleBtn.addEventListener('click', () => {
-    console.log('clicked');
-    lightToggleBtn.classList.toggle('active');
-    lightToggleBtn.innerHTML = lightToggleBtn.classList.contains('active') ? '☽' : '☼';
-    document.querySelectorAll('section').forEach(section => {
-        section.classList.toggle('dark-mode');
-    })
-});
-
-//projects page slide btns
-const sectionContent = document.querySelectorAll('section');
-
-sectionContent.forEach(content => {
+// Projects Page Slide Buttons
+sections.forEach(content => {
     let title = content.querySelector('h1');
     let leftBtn = content.querySelector('.left-btn');
     let rightBtn = content.querySelector('.right-btn');
